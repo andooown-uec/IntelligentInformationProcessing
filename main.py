56b88b291b85fa9bef2561fb8b6df30cd3f8db13
andooown <andooown.dev@gmail.com>
2017-12-07 21:49:32 +0900

    遺伝子と巡回する順番を相互に変換する関数を追加

--- main.py
+++ main.py
@@ -2,6 +2,39 @@
 
 import numpy as np
 
+pos_list = []   # 巡回する地点のリスト
+pos = []        # 巡回する地点の座標
+
+
+def encode_gene(order):
+    """各地点を巡回する順番を遺伝子に変換する関数"""
+    # 巡回する地点のリストをコピー
+    cp_list = pos_list.copy()
+    # 遺伝子を生成
+    gene = []
+    for o in order:
+        # 位置を検索
+        index = cp_list.index(o)
+        # 遺伝子に追加し、地点のリストから削除
+        gene.append(index)
+        cp_list.pop(index)
+
+    return gene
+
+
+def decode_gene(gene):
+    """遺伝子を各地点を巡回する順番に変換する関数"""
+    # 巡回する地点のリストをコピー
+    cp_list = pos_list.copy()
+    # 遺伝子を生成
+    order = []
+    for g in gene:
+        # 遺伝子から位置を検索し、地点のリストに追加
+        order.append(cp_list.pop(g))
+
+    return order
+
+
 if __name__ == '__main__':
     pos_count = 32  # 巡回する地点の数
 
