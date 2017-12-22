e946caf4d7f9e17c1eb3f6b10ab24f46554bb4d6
Ando Yoshikazu <andooown.dev@gmail.com>
2017-12-23 01:03:29 +0900

    順序交叉を実装

--- crossover.py
+++ crossover.py
@@ -22,3 +22,24 @@ def cycle_crossover(ind1, ind2):
             new1[i], new2[i] = old2.pop(0), old1.pop(0)
 
     return new1, new2
+
+
+def order_crossover(ind1, ind2):
+    """順序交叉"""
+    size = len(ind1)  # 個体の大きさ
+    # 切断点を決定する
+    p1 = np.random.randint(0, size - 1)
+    p2 = np.random.randint(p1 + 1, size)
+    # 切断点間はそのままコピー
+    new1, new2 = ind1[p1:p2], ind2[p1:p2]   # 子孫
+    # 第 2 切断点を先頭に並べ替え
+    ind1 = ind1[p2:] + ind1[:p1] + ind1[p1:p2]
+    ind2 = ind2[p2:] + ind2[:p1] + ind2[p1:p2]
+    # 他方の子と衝突するものを除く
+    ind1 = list(filter(lambda x: x not in new2, ind1))
+    ind2 = list(filter(lambda x: x not in new1, ind2))
+    # 残りを継承
+    new1 = ind2[size - p2:] + new1 + ind2[:size - p2]
+    new2 = ind1[size - p2:] + new2 + ind1[:size - p2]
+
+    return new1, new2
